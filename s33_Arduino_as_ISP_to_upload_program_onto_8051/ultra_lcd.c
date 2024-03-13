fec3a7fed8528672fe5126bda9b3ac457c1f8d24
Ashok Tirupatimahanti <hash.oak9@gmail.com>
2024-03-13 18:56:43 +0530

    update functionalities

--- s33_Arduino_as_ISP_to_upload_program_onto_8051/ultra_lcd.c
+++ s33_Arduino_as_ISP_to_upload_program_onto_8051/ultra_lcd.c
@@ -1,4 +1,5 @@
 #include <reg51.h>
+#include <math.h>
 #include <intrins.h>
 
 #define lcd_data P2
@@ -7,7 +8,9 @@ sfr16 DPTR = 0x82;
 
 sbit trig = P3^5;
 sbit echo = P3^2;
-sbit led = P3^1;
+sbit led = P1^1;
+
+short dist=0;
 
 sbit rs = P0^0;
 sbit rw = P0^1;
@@ -21,18 +24,25 @@ void lcd_delay();
 
 void lcd_init()
 {
-    cmd(0x38);
-    cmd(0x0e);
+	cmd(0x02);
+    cmd(0x28);
     cmd(0x06);
     cmd(0x0c);
+	cmd(0x01);
     cmd(0x80);
 }
 
 void cmd(unsigned char a)
 {
-    lcd_data = a;
+    lcd_data = (a & 0xF0);
     rs = 0;
     rw = 0;
+    en = 1;
+    lcd_delay();
+    en = 0;
+	lcd_delay();
+	
+	lcd_data = (a << 4);
     en = 1;
     lcd_delay();
     en = 0;
@@ -40,9 +50,15 @@ void cmd(unsigned char a)
 
 void dat(unsigned char b)
 {
-    lcd_data = b;
+    lcd_data = (b & 0xF0);
     rs = 1;
     rw = 0;
+    en = 1;
+    lcd_delay();
+    en = 0;
+	lcd_delay();
+	
+	lcd_data = (b << 4);
     en = 1;
     lcd_delay();
     en = 0;
@@ -50,17 +66,13 @@ void dat(unsigned char b)
 
 void show(unsigned char *s)
 {
-    while (*s)
-    {
-        dat(*s++);
-    }
+    while (*s) dat(*s++);
 }
 
 void lcd_delay()
 {
     unsigned int i;
-    for (i = 0; i <= 1000; i++)
-        ;
+    for (i = 0; i <= 1000; i++);
 }
 
 void send_pulse(void)
@@ -72,49 +84,48 @@ void send_pulse(void)
     trig = 0;
 }
 
-unsigned char ultrasonic()
+short ultrasonic()
 {
-    unsigned char get;
+    short get;
     send_pulse();
-    while (!echo)
-        ;
-    while (echo)
-        ;
+    while (!echo);
+    while (echo);
     DPH = TH0;
     DPL = TL0;
     TH0 = TL0 = 0xff;
-    if (DPTR < 38000)
-        get = DPTR / 59;
-    else
-        get = 0;
+    if (DPTR < 38000) get = DPTR / 59;
+    else get = 0;
     return get;
 }
 
 void main()
 {
+	char i;
     TMOD = 0x09;
     TH0 = TL0 = 0x00;
     TR0 = 1;
     lcd_init();
-    cmd(0x84);
-    show("Distance");
-    cmd(0xc9);
+    cmd(0x81);
+    show("Distance:");
+    cmd(0x8d);
     show("cm");
     P3 |= (1 << 2);
     while (1)
     {
-        short dist;
-        char i;
         dist = ultrasonic();
-        if (dist < 9)
-            led = 1;
-        else
-            led = 0;
-        cmd(0xc5);
-        for (i = 4; i; i--)
-        {
-            dat('0' + dist % 10);
-            dist /= 10;
-        }
+        if (dist > 400){
+			led = 0;
+			show("---");
+			continue;
+		}
+        led = 1;
+		cmd(0x8a);
+		dat((dist/100)%100+48);
+		dat((dist/10)%10+48);
+		dat((dist%10)+48);
+		dist=sqrt(dist*1.5625);
+		cmd(0xc0);
+		for(i=1;i<=16;i++)
+			i<=dist ? dat(255):dat(' ');
     }
 }
