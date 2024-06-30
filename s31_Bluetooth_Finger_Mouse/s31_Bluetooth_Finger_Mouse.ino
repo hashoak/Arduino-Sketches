1448e282fa341c18e26d5021aa80741e341e2799
Ashok Tirupatimahanti <hash.oak9@gmail.com>
2024-06-30 13:32:50 +0530

    Update s31_Bluetooth_Finger_Mouse.ino

--- s31_Bluetooth_Finger_Mouse/s31_Bluetooth_Finger_Mouse.ino
+++ s31_Bluetooth_Finger_Mouse/s31_Bluetooth_Finger_Mouse.ino
@@ -6,17 +6,17 @@
 Adafruit_MPU6050 mpu;
 BleMouse bleMouse("Hash's Mouse", "Hash_Oak", 100);
 
-int inpin1 = 15;   // Touch input detect pin 1
-int inpin2 = 2;    // Touch input detect pin 2
-int inpin3 = 4;    // Touch input detect pin 3
-int outpin1 = 39;  // Touch output detect pin 1
-int outpin2 = 36;  // Touch output detect pin 2
+int outpin1 = 36;  // Touch output detect pin 1
+int outpin2 = 39;  // Touch output detect pin 2
+int inpin1 = 34;   // Touch input detect pin 1
+int inpin2 = 35;    // Touch input detect pin 2
+int inpin3 = 32;    // Touch input detect pin 3
 
 int mt = 500;
-int ut = 3000;      // Touch upper threshold value
-int lt = 1000;      // Touch lower threshold value
+int ut = 2500;      // Touch upper threshold value
+int lt = 1500;      // Touch lower threshold value
 int wt = 100;       // Wait time for next reading
-int cf = 7;         // Cursor multipier factor
+int cf = 15;         // Cursor multipier factor
 float st = 1 / cf;  // Sensor movement threshold value
 
 void setup() {
@@ -31,11 +31,11 @@ void setup() {
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
+  pinMode(outpin1, INPUT);
+  pinMode(outpin2, INPUT);
   pinMode(inpin1, INPUT);
   pinMode(inpin2, INPUT);
   pinMode(inpin3, INPUT);
-  pinMode(outpin1, INPUT);
-  pinMode(outpin2, INPUT);
 
   Serial.println("Starting BLE...");
   bleMouse.begin();
@@ -43,59 +43,61 @@ void setup() {
   Serial.println("BLE Connected...");
 }
 
-int i1, i2, i3, o1, o2;
+int o1, o2, i1, i2, i3;
 
 void loop() {
+  // delay(wt);
   // cursor();
+  // return;
   if (!bleMouse.isConnected()) return;
+  o1 = analogRead(outpin1);
+  o2 = analogRead(outpin2);
   i1 = analogRead(inpin1);
   i2 = analogRead(inpin2);
   i3 = analogRead(inpin3);
-  o1 = analogRead(outpin1);
-  o2 = analogRead(outpin2);
-  // return;
   if (i1 < mt && i2 < mt && i3 < mt) return;
-  Serial.printf("%d\t%d\t%d\t%d\t%d\n", i1, i2, i3, o1, o2);
+  // Serial.print("rl:0,ru:4096,");
+  // Serial.printf("o1:%d,o2:%d,i1:%d,i2:%d,i3:%d\n", o1, o2, i1, i2, i3);
   if (i1 > lt && o1 < ut) {
     Serial.println("Left click");
     Serial.printf("%d\t%d\n", i1, o1);
     bleMouse.press(MOUSE_LEFT);
+    delay(wt);
     while (analogRead(inpin1) > lt) cursor();
     bleMouse.release(MOUSE_LEFT);
-    delay(wt);
   } else if (i2 > lt && o1 < ut) {
     Serial.println("Cursor");
     Serial.printf("%d\t%d\n", i2, o1);
-    while (analogRead(inpin2) > lt) cursor();
     delay(wt);
+    while (analogRead(inpin2) > lt) cursor();
   } else if (i3 > lt && o1 < ut) {
     Serial.println("Right click");
     Serial.printf("%d\t%d\n", i3, o1);
     bleMouse.press(MOUSE_RIGHT);
+    delay(wt);
     while (analogRead(inpin3) > lt) cursor();
     bleMouse.release(MOUSE_RIGHT);
-    delay(wt);
   } else if (i1 > lt && o2 < ut) {
     Serial.print("Extra 1  ");
     Serial.printf("%d\t%d\n", i1, o2);
     bleMouse.press(MOUSE_BACK);
+    delay(wt);
     while (analogRead(inpin1) > lt) cursor();
     bleMouse.release(MOUSE_BACK);
-    delay(wt);
   } else if (i2 > lt && o2 < ut) {
     Serial.print("Extra 2  ");
     Serial.printf("%d\t%d\n", i2, o2);
     bleMouse.press(MOUSE_MIDDLE);
+    delay(wt);
     while (analogRead(inpin2) > lt) cursor();
     bleMouse.release(MOUSE_MIDDLE);
-    delay(wt);
   } else if (i3 > lt && o2 < ut) {
     Serial.print("Extra 3  ");
     Serial.printf("%d\t%d\n", i3, o2);
     bleMouse.press(MOUSE_FORWARD);
+    delay(wt);
     while (analogRead(inpin3) > lt) cursor();
     bleMouse.release(MOUSE_FORWARD);
-    delay(wt);
   }
 }
 
@@ -103,11 +105,10 @@ sensors_event_t a, g, temp;
 
 void cursor() {
   mpu.getEvent(&a, &g, &temp);
-  // Serial.print(g.gyro.x);
-  // Serial.print("\t");
-  // Serial.print(g.gyro.y);
-  // Serial.print("\t");
-  // Serial.println(g.gyro.z);
+  // Serial.print("rl:-10,ru:10,");
+  // Serial.printf("acc_x:%f,acc_y:%f,acc_z:%f,gyr_x:%f,gyr_y:%f,gyr_z:%f\n",
+  //               a.acceleration.x,a.acceleration.y,a.acceleration.z,
+  //               g.gyro.x,g.gyro.y,g.gyro.z);
   if (g.gyro.z < -st) Serial.println("Move Up");
   if (g.gyro.z > st) Serial.println("Move Down");
   if (g.gyro.y < -st) Serial.println("Move Left");
